69644587643bf3860aac2aa4b7d7d0463e381c3d
Obinna Igwe <igwenus619@gmail.com>
2019-10-10 06:39:26 +0100

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:DartProjects/dummy.dart' as dummy;
+
+main(List<String> arguments) {
+  print('Hello world: ${dummy.calculate()}!');
+}
---
+++ test/test.dart
@@ -0,0 +1,8 @@
+import 'package:DartProjects/dummy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

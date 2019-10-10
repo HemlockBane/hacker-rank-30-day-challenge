4d25d88a94109ec3705bdfe508a14d414b9b07dc
Obinna Igwe <igwenus619@gmail.com>
2019-10-10 16:33:42 +0100

    Get a text from console, save it to a variable, print 'Hello, World.', then print the variable on a new line

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,5 @@
-import 'package:DartProjects/dummy.dart' as dummy;
+import 'package:DartProjects/hello_world.dart';
 
 main(List<String> arguments) {
-  print('Hello world: ${dummy.calculate()}!');
+ printHelloWorld();
 }

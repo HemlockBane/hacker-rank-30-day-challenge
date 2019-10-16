02fb44d4c867bac4e91c84954718cdc3f1ce4241
Obinna Igwe <igwenus619@gmail.com>
2019-10-16 09:11:01 +0100

    Reformat comment

--- lib/data_types.dart
+++ lib/data_types.dart
@@ -21,7 +21,9 @@ import 'dart:io';
 
 //Output Format:
 //
-//Print the sum of both integers on the first line, the sum of both doubles (scaled to  decimal place) on the second line, and then the two concatenated strings on the third line.
+//Print the sum of both integers on the first line, the sum of both doubles
+// (scaled to  decimal place) on the second line, and then the two concatenated
+// strings on the third line.
 
 void printSumOfDataTypes() {
   int i = 2;
